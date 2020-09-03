4c5923db8533426814342104e521c7ec045c537e
Joseph Sawaya <josephsawaya938@gmail.com>
2020-09-02 23:46:49 -0400

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -25,43 +25,72 @@ mod types {
 }
 
 mod args {
-    pub fn new() -> () {
+    pub fn new() {
+        use std::process::Command;
         let name = dialoguer::Input::<String>::new()
             .with_prompt("Your Project Name:")
             .interact()
             .expect("Expected an argument");
         let mut path_name = String::from("./");
         path_name.push_str(&name);
-        let list = vec!["@bgpc/aws-serverless-toolkit", "typescript", "ts-node"];
-        use cmd_lib::run_cmd;
-        if run_cmd! {
-            mkdir $path_name;
-            cd $path_name;
-            yarn init -y || npm init -y;
-        }
-        .is_err()
-        {
-            panic!("Error! Make sure you have npm or yarn installed.");
-        }
-        for arg in &list {
-            println!("{}", path_name);
-            if run_cmd! {
-                cd $path_name;
-                yarn add -D $arg || npm add -d $arg;
-            }
-            .is_err()
-            {
-                panic!("Error! Make sure you have npm or yarn installed.");
-            }
-        }
-        if run_cmd! {
-            cd $path_name;
-            yarn add -D npm-add-script || npm add -d npm-add-script;
-            npx npm-add-script -k "configure:dev" -v "NODE_ENV=development ts-node -r dotenv/config src/configure.ts";
-        }
-        .is_err()
-        {
-            panic!("Error! Make sure you have npm or yarn installed.");
-        }
+        const LINK: &str = "https://github.com/josephsawaya/aws-serverless-toolkit-template.git";
+        if cfg!(windows) {
+            let mut clone = String::from("git clone ");
+            clone.push_str(&LINK);
+            clone.push_str(" ");
+            clone.push_str(&path_name);
+            let mut child = Command::new("cmd")
+                .args(&["/C", &clone[..]])
+                .spawn()
+                .expect("failed to execute process");
+            child.wait().expect("failed to execute process");
+
+            let mut cd = String::from("cd ");
+            cd.push_str(&path_name);
+            child = Command::new("cmd")
+                .args(&["/C", &cd[..]])
+                .spawn()
+                .expect("failed to execute process");
+            child.wait().expect("failed to execute process");
+
+            std::env::set_current_dir(&path_name).expect("failed to execute process");
+            child = Command::new("cmd")
+                .args(&["/C", "RMDIR .\\git /S"])
+                .spawn()
+                .expect("failed to execute process");
+            child.wait().expect("failed to execute process");
+
+            child = Command::new("cmd")
+                .args(&["/C", "git init"])
+                .spawn()
+                .expect("failed to execute process");
+            child.wait().expect("failed to execute process");
+        } else {
+            let mut clone = String::from("git clone ");
+            clone.push_str(&LINK);
+            clone.push_str(" ");
+            clone.push_str(&path_name);
+            let mut child = Command::new("sh")
+                .arg("-c")
+                .arg(clone)
+                .spawn()
+                .expect("failed to execute process");
+            child.wait().expect("failed to execute process");
+            let mut cd = String::from("cd ");
+            cd.push_str(&path_name);
+            std::env::set_current_dir(&path_name).expect("failed to execute process");
+            child = Command::new("sh")
+                .arg("-c")
+                .arg("rm -rf ./.git")
+                .spawn()
+                .expect("failed to execute process");
+            child.wait().expect("failed to execute process");
+            child = Command::new("sh")
+                .arg("-c")
+                .arg("git init")
+                .spawn()
+                .expect("failed to execute process");
+            child.wait().expect("failed to execute process");
+        };
     }
 }
